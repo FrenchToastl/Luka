eae51d4ca5475a575a0684c2ea9d53cd0ea22b3f
FrenchToastl <65655176+FrenchToastl@users.noreply.github.com>
2020-06-02 03:23:50 +0400

    Add files via upload
    Last one

--- Website1/main.js
+++ Website1/main.js
@@ -10,15 +10,20 @@ if (document.getElementsByClassName('btn-order') !== null) {
     }
 }
 
+
+
+
 if (document.getElementById('order') !== null) {
     let orderList = document.getElementById('order'),
         price = 0;
     for (let i = 0; i < localStorage.length; i++) {
         price += parseInt(localStorage.getItem(localStorage.key(i)).split(" Gel")[0]);
-        orderList.insertAdjacentHTML('beforeend', '<div>' + localStorage.key(i) + ' ' + localStorage.getItem(localStorage.key(i)) + '</br></div>');
+        orderList.insertAdjacentHTML('beforeend', "<div style='border-bottom: solid 1px gray'>" +
+                                                                "<button type='button' class='close' onclick='localStorage.removeItem(localStorage.key(" + i + ")); this.parentElement.remove()'" + "aria-label='Close'>" +
+                                                                    "<span aria-hidden='true'>&times;</span>"
+                                                                + "</button>" + localStorage.key(i) + " " + localStorage.getItem(localStorage.key(i)) + "</br></div>");
     }
     orderList.insertAdjacentHTML('beforeend', '<div>Total Price: ' + price + '</br></div>');
-
 }
 
 
